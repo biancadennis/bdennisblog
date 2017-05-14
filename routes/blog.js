8f704a58d73490eb44e43235b8afc1ac228ef942
Bianca Dennis <bianca.dennis@caa.columbia.edu>
2017-05-14 13:48:50 -0400

    database working

--- routes/blog.js
+++ routes/blog.js
@@ -1,22 +1,28 @@
 var express = require('express');
-var Post = require('../models/post');
 var router = express.Router();
+
+var models = require('../models/index');
+var Post = models.post;
 
 //Index
 router.get('/', function(request, response){
-    response.render('blogIndex')
-})
+    Post.findAll().then(function(posts) {
+        response.render('blogIndex', {
+            posts:posts
+        });
+    });
+});
 
 //Create
-router.post('/', function (request, response){
+router.post('/', function(request, response) {
     Post.create({
         title: request.body.title,
         body: request.body.body,
         slug: request.body.slug
     }).then(function(post){
         response.redirect(post.url);
-    });
-});
+    })
+})
 
 // New
 router.get('/new', function(request, response){
